91a61d16dc31763094c0bbead468f3e7295e91be
Donething <donething.net@gmail.com>
2021-12-24 18:42:07 +0800

    添加音量增强器

--- public/scripts/volume_enhance.js
+++ public/scripts/volume_enhance.js
@@ -1,50 +1,53 @@
 // [javascript - HTML5 Volume Increase Past 100% - Stack Overflow]
 // (https://stackoverflow.com/questions/43794356/html5-volume-increase-past-100)
-!function () {
-  // 查找媒体元素
-  // 先在首层document中查找媒体元素
-  let media = findLargestPlayingVideo(document) || document.querySelector("audio");
-  if (!media) {
-    // 首层没有找到时，从iframe中查找
-    let iframes = document.querySelectorAll("iframe");
-    for (const f of iframes) {
-      media = findLargestPlayingVideo(f.contentWindow.document)
-        || document.querySelector("audio");
-      // 已找到，退出查找
-      if (media) {
-        break;
+if (typeof window.enhanceVolume !== "function") {
+  /**
+   * 设置音量增强的值
+   * @param {number} [volumeEnhanceValue] 当值为空时，发送当前页面的媒体之前被设置的值
+   */
+  window.enhanceVolume = function (volumeEnhanceValue) {
+    // 查找媒体元素
+    // 先在首层document中查找媒体元素
+    let media = findLargestPlayingVideo(document) || document.querySelector("audio");
+    if (!media) {
+      // 首层没有找到时，从iframe中查找
+      let iframes = document.querySelectorAll("iframe");
+      for (const f of iframes) {
+        media = findLargestPlayingVideo(f.contentWindow.document) || document.querySelector("audio");
+        // 已找到，退出查找
+        if (media) {
+          break;
+        }
       }
     }
-  }
 
-  // 最终都没有找到媒体元素时，退出执行
-  if (!media) {
-    console.log("未发现媒体元素，退出音量增强器");
-    // -1 为没有找到的标志
-    chrome.runtime.sendMessage({cmd: "volume", volume: -1});
-    return;
-  }
+    // 最终都没有找到媒体元素时，退出执行
+    if (!media) {
+      console.log("[EnVol] 未发现媒体元素，退出音量增强器");
+      // -1 为没有找到媒体元素的标志
+      chrome.runtime.sendMessage({cmd: "volumeEnhance", volumeEnhanceValue: -1});
+      return;
+    }
 
-  // 返回或修改媒体的音量增强
-  if (!window.audioCtx) {
-    // create an audio context and hook up the video element as the source
-    window.audioCtx = new AudioContext();
-    window.source = audioCtx.createMediaElementSource(media);
+    // 返回或修改媒体的音量增强
+    if (!window.audioCtx) {
+      // create an audio context and hook up the video element as the source
+      window.audioCtx = new AudioContext();
+      window.source = audioCtx.createMediaElementSource(media);
 
-    // create a gain node
-    window.gainNode = window.audioCtx.createGain();
-    window.source.connect(window.gainNode);
+      // create a gain node
+      window.gainNode = window.audioCtx.createGain();
+      window.source.connect(window.gainNode);
 
-    // connect the gain node to an output destination
-    window.gainNode.connect(window.audioCtx.destination);
-  }
+      // connect the gain node to an output destination
+      window.gainNode.connect(window.audioCtx.destination);
+    }
 
-  // Enhance the volume
-  if (typeof volume === "undefined" || !volume) {
-    chrome.runtime.sendMessage({cmd: "volume", volume: window.gainNode.gain.value});
-  } else {
-    window.gainNode.gain.value = volume;
-    // 每次运行后设volume为 null，以判断是获取还是设置 volume
-    volume = null;
-  }
-}();
+    // Enhance the volume
+    if (typeof volumeEnhanceValue === "undefined" || !volumeEnhanceValue) {
+      chrome.runtime.sendMessage({cmd: "volumeEnhance", volumeEnhanceValue: window.gainNode.gain.value});
+    } else {
+      window.gainNode.gain.value = volumeEnhanceValue;
+    }
+  };
+}
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,43 +1,100 @@
-import React, {useEffect} from 'react'
-import {Button, Space} from "antd"
+import React, {useEffect, useState} from 'react'
+import {Slider, Space, Tag} from "antd"
+import Icon from '@ant-design/icons'
+import {ReactComponent as IconVolume} from "../../icons/volume.svg"
+import {ReactComponent as IconHot} from "../../icons/hot.svg"
+import {ReactComponent as IconBili} from "../../icons/bili.svg"
+import {ReactComponent as IconVideo} from "../../icons/video.svg"
+import {ReactComponent as IconHeaders} from "../../icons/headers.svg"
+import {ReactComponent as IconVideoTool} from "../../icons/video_tool.svg"
+import {ReactComponent as IconTV} from "../../icons/tv.svg"
+import {ReactComponent as IconOptions} from "../../icons/options.svg"
 
 const Popup = function () {
+  // 音量增强值
+  const [volumeEnhance, setVomumeEnhance] = useState(1)
+
   useEffect(() => {
     document.title = `弹出框 - ${chrome.runtime.getManifest().name}`
+
+    // 接收当前页面的媒体元素的音量增强值
+    chrome.runtime.onMessage.addListener(message => {
+      if (message.cmd === "volumeEnhance") {
+        // 当 volumeEnhanceValue为 -1 时表示在当前页面中没有找到媒体元素，
+        console.log("收到消息，设置音量增强值：", message.volumeEnhanceValue)
+        setVomumeEnhance(message.volumeEnhanceValue)
+      }
+    })
+
+    // 让当前页面的内容脚本发送媒体元素的音量增强值
+    chrome.tabs.query({active: true, currentWindow: true}).then(async ([tab]) => {
+      if (!tab.id) {
+        return
+      }
+      // 引入脚本
+      await chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ["/scripts/volume_enhance.js"]
+      })
+
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        // @ts-ignore
+        func: () => window.enhanceVolume()
+      })
+    })
   }, [])
 
   return (
-    <Space direction="vertical" style={{width: 80, padding: 5}}>
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/hot_topics"})}>热帖
+    <Space direction="vertical" style={{width: 100, padding: 5}}>
+      <span className="row align-center">
+        <Icon component={IconVolume}/>
+        <Slider className="width-fill-remain margin-h-large" min={0} max={10} step={0.1} value={volumeEnhance}
+                disabled={volumeEnhance === -1} onChange={v => {
+          setVomumeEnhance(v)
+          chrome.tabs.query({active: true, currentWindow: true}).then(([tab]) => {
+            if (!tab.id) {
+              return
+            }
+            chrome.scripting.executeScript({
+              target: {tabId: tab.id},
+              // @ts-ignore
+              func: (volEn) => window.enhanceVolume(volEn),
+              args: [volumeEnhance]
+            })
+          })
+        }}/>
       </span>
 
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/bili_video"})}>哔哩视频
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/hot_topics"})}>
+        <Icon component={IconHot}/> 热帖
       </span>
 
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/video_fav"})}>视频收藏
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/bili_video"})}>
+        <Icon component={IconBili}/> 哔哩视频
       </span>
 
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/http_headers"})}>转请求头
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/video_fav"})}>
+        <Icon component={IconVideo}/> 视频收藏
       </span>
 
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/video_tool"})}>视频工具
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/http_headers"})}>
+        <Icon component={IconHeaders}/> 转请求头
       </span>
 
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/iptv"})}>IPTV源
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/video_tool"})}>
+        <Icon component={IconVideoTool}/> 视频工具
       </span>
 
-      <span className="clickable" onClick={() =>
-        chrome.tabs.create({url: "/index.html#/options"})}>选项
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/iptv"})}>
+        <Icon component={IconTV}/> IPTV源
+      </span>
+
+      <span className="clickable" onClick={() => chrome.tabs.create({url: "/index.html#/options"})}>
+        <Icon component={IconOptions}/> 选项
       </span>
     </Space>
   )
 }
 
 export default Popup
-
