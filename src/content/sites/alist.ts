cca33c0dc13c852e7f76ca0c34ff0873ac3395dc
Donething <donething.net@gmail.com>
2022-12-11 17:25:46 +0800

    在视频列表页，也不是播放页获取视频列表。以免在有些不知道的视频格式的页面中，无法下载列表

--- src/content/sites/alist.ts
+++ src/content/sites/alist.ts
@@ -5,34 +5,32 @@ import {M3uPlaylist, M3uMedia} from 'm3u-parser-generator'
 import {sleep} from "do-utils"
 import {download} from "do-utils/dist/elem"
 
+const TAG = "[CEXT]"
+
+// 视频格式
+const VIDEO_EXT = ".avi.mp4.mkv.ts"
+// Alist 运行端口
+const ALIST_PORT = 5244
+
 // 获取 Alist 的播放列表
 const genAlistPlaylist = async () => {
   // 为管理页面
   if (window.location.pathname === "/@manage") {
-    console.log("为管理页面，退出获取播放列表")
+    console.log(TAG, "为管理页面，退出获取播放列表")
     return
   }
 
   // 需要现在页面中显示播放列表，脚本才能获取到播放列表
-  let showPlaylistBn: Element | undefined | null = undefined
+  let toolsNav: Element | undefined | null = undefined
   while (true) {
-    showPlaylistBn = document.querySelector("div#video-player")?.nextElementSibling
-    if (showPlaylistBn) {
+    toolsNav = document.querySelector("div.hope-stack")
+    if (toolsNav) {
       break
     }
-    // 为目录页面，退出
-    if (document.querySelector("div.title p")?.textContent === "名称") {
-      console.log("为目录页面，退出获取播放列表")
-      return
-    }
 
-    // console.log("还需等待显示播放列表的按钮")
+    // console.log(TAG, "还需等待显示播放列表的按钮")
     await sleep(100)
   }
-  console.log("已出现显示播放列表的按钮，将添加下载按钮")
-
-  // 显示播放列表按钮
-  let bnShow = showPlaylistBn as HTMLButtonElement
 
   // 添加下载按钮
   // 已存在，直接返回
@@ -40,65 +38,67 @@ const genAlistPlaylist = async () => {
 
   let bnDown = document.createElement("button")
   bnDown.id = "do_bn_down_playlist"
-  bnDown.textContent = "保存列表"
+  bnDown.textContent = "下载列表"
   bnDown.title = "下载播放列表为'.m3u8'文件"
-  bnDown.style.padding = "5px"
   bnDown.style.cursor = "pointer"
+  bnDown.style.padding = "5px"
+  bnDown.style.marginRight = "16px"
+  bnDown.style.marginLeft = "auto"
 
   bnDown.onclick = () => {
-    bnShow.click()
     // 获取播放列表所在的父元素
-    let listParent = document.querySelector('div[role="listbox"]')
-    if (!listParent) {
-      console.log("无法获取播放列表所在的父元素")
+    let videoElems = document.querySelectorAll("a.list-item")
+    if (videoElems.length === 0) {
+      console.log(TAG, "该页面中无法获取到视频列表")
       return
     }
+
+    // 视频集的名字
+    let albumName = document.querySelector("nav.nav ol")?.lastChild?.textContent || "未知的视频集名"
 
     // 构造播放列表
     const playlist = new M3uPlaylist()
     playlist.title = "播放列表"
 
-    // 获取当前视频目录的播放地址 dirAddr
-    let potLink = document.querySelector("a[href^='potplayer://']")
-    if (!potLink) {
-      console.log("Potplay 链接元素为空，无法获取到视频目录的播放地址")
+    // 添加播放列表
+    let links = Array.from(videoElems).filter(item => {
+      // 从链接中提取为视频的链接（根据后缀格式判断）
+      let href = (item as HTMLLinkElement).href
+      let name = href.substring(href.lastIndexOf("/"))
+      if (name.indexOf(".") === -1) {
+        return false
+      }
+
+      let ext = name.substring(name.lastIndexOf("."))
+      return VIDEO_EXT.indexOf(ext) !== -1
+    }).map(item => (item as HTMLLinkElement).href || "未知的文件地址")
+
+    if (links.length === 0) {
+      console.log(TAG, "该页面中没有找到视频")
       return
     }
-    let path = (potLink as HTMLLinkElement).href.replace("potplayer://", "")
-    path = decodeURIComponent(decodeURIComponent(path))
-    let dirAddr = path.substring(0, path.lastIndexOf("/") + 1)
-    let filename = path.substring(path.lastIndexOf("/") + 1)
-    filename = filename.substring(0, filename.indexOf("."))
 
-    // 添加播放列表
-    let names = Array.from(listParent.children).map(item => item.textContent || "未知的文件名")
-    names = names.sort((a, b) => a.localeCompare(b))
-    for (let name of names) {
-      const media = new M3uMedia(dirAddr + name)
-      media.name = name
+    // 需要先排序
+    links = links.sort((a, b) => a.localeCompare(b))
+    for (let link of links) {
+      const decodeLink = decodeURIComponent(link)
+      const media = new M3uMedia(decodeLink.replace(`:${ALIST_PORT}/`, `:${ALIST_PORT}/d/`))
+      media.name = decodeLink.substring(decodeLink.lastIndexOf("/") + 1)
 
       playlist.medias.push(media)
     }
 
     // 提取显示播放列表
     const m3uString = playlist.getM3uString()
-    download(m3uString, `${filename}.m3u8`)
-
-    // 隐藏播放列表
-    bnShow.click()
+    download(m3uString, `${albumName}.m3u8`)
   }
 
   // 添加按钮
-  let tools = bnShow.nextElementSibling
-  if (!tools) {
-    console.log("没有找到工具栏，无法添加下载按钮")
-    return
-  }
-  tools.appendChild(bnDown)
+  toolsNav.insertBefore(bnDown, toolsNav.lastChild)
 }
 
 const start = async () => {
-  console.log("开始获取`Alist`的播放列表")
+  console.log(TAG, "开始添加下载视频列表的功能")
   genAlistPlaylist()
 }
 
